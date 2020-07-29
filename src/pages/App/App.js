2a595e19ca7f69405b5f9b284fe3801d5a60d48f
David Sheinbein <davesheinbein@gmail.com>
2020-07-29 12:34:14 -0700

    init commit

---
+++ src/pages/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+class App extends Component {
+ /*--- State ---*/
+ /*--- Handle Methods ---*/
+ /*--- Lifecycle Methods ---*/
+ render() {
+   return (
+     <div className="App">
+       <header className="App-header">MY APP</header>
+       <main>
+         Body
+       </main>
+       <footer>Footer</footer>
+     </div>
+   );
+ }
+}
+export default App;

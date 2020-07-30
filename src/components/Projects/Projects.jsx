5098ac780898cb84a9dfb4798998f69fc152e7cc
David Sheinbein <davesheinbein@gmail.com>
2020-07-29 20:55:24 -0700

    projects component updated

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,11 +1,122 @@
-import React from 'react'
+import React, { Component } from "react";
+import "./Projects.css";
+import steadfinder from "../../images/steadfinder.png";
+import inspirations from "../../images/inspirations.png";
+import twistsofrate from "../../images/twistsofrate.png";
+import wargame from "../../images/wargame.png";
 
-function Projects() {
-    return (
-        <div>
-            Projects
-        </div>
-    )
+export default class Projects extends Component {
+    render() {
+        return (
+            <div className="projects">
+                <div className='projectTitle'>A few past projects</div>
+                <div className="post-wrapper">
+                    <div>
+                        <div className="post">
+                            <img
+                                className="thumbnail"
+                                alt="checkers"
+                                src={steadfinder}>
+                            </img>
+                            <div className="post-preview">
+                                <div className="post-title">Stead Finder</div>
+                                <p className="post-intro">
+                                    A horse marketplace bulletin board. People can post horses
+                                    they have for sale and conact people directly to inquire about
+                                    buying a horse.
+
+                                </p>
+                                <div className='viewMoreBtn'>
+                                    <a
+                                        href="https://github.com/davesheinbein/stead-finder-horse-buliten-board"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        View More!
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="post">
+                            <img
+                                className="thumbnail"
+                                alt="dark-game-secrets"
+                                src={inspirations}
+                            ></img>
+                            <div className="post-preview">
+                                <div className="post-title">Inspirations</div>
+                                <p className="post-intro">
+                                    A repository of Gifs and Videos. People can post GIF, Videos,
+                                    and comments. Utilizing full CRUD functionality.
+                                </p>
+                                <div className='viewMoreBtn'>
+                                    <a
+                                        href="https://github.com/davesheinbein/full-stack-web-app"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        View More!
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="post">
+                            <img
+                                className="thumbnail"
+                                alt="twistsofrate"
+                                src={twistsofrate}
+                            ></img>
+                            <div className="post-preview">
+                                <div className="post-title">TwistsOfRate</div>
+                                <p className="post-intro">
+                                    A site with detailed information about games, genres, consoles
+                                    and the ability for users to blog about their favorite games.
+
+                                </p>
+                                <div className='viewMoreBtn'>
+                                    <a
+                                        href="https://github.com/davesheinbein/TwistsOfRate"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        View More!
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="post">
+                            <img
+                                className="thumbnail"
+                                alt="pokemon-creations"
+                                src={wargame}
+                            ></img>
+                            <div className="post-preview">
+                                <div className="post-title">War Game</div>
+                                <p className="post-intro">
+                                    A easy to play game based on the card game war. Face off
+                                    against against a computer. May the odd be in your favor.
+
+                                </p>
+                                <div className='viewMoreBtn'>
+                                    <a
+                                        href="https://github.com/davesheinbein/War-game"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        View More!
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
-
-export default Projects

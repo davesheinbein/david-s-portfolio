e58c954acfb4fb56335d5696fef684ccee203256
David Sheinbein <davesheinbein@gmail.com>
2020-07-29 21:36:54 -0700

    react-router-dom module installed

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages/App/App';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
+  <Router>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Route render={({ history }) => <App history={history} />} />
+  </React.StrictMode>
+</Router>,
   document.getElementById('root')
 );
 

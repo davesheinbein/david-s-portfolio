4b69a66fb6a6a9f42a51f1608845d471a15d2cb7
David Sheinbein <davesheinbein@gmail.com>
2020-08-11 14:26:25 -0700

    update to app.js

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import SmokePage from '../SmokePage/SmokePage';
 import Layout from '../LayoutPage/LayoutPage';
-import Fullpageloader from '../../components/FullPageLoader/FullPageLoader';
 import { createGlobalStyle } from 'styled-components';
 import { Route, Switch } from 'react-router-dom';
 
@@ -413,7 +412,6 @@ class App extends React.Component {
 						)}
 					/>
 				</Switch>
-				{/* <Fullpageloader /> */}
 			</div>
 		);
 	}

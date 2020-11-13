429b55e95eae530de947432866ae2ccd8283dd20
David Sheinbein <davesheinbein@gmail.com>
2020-11-12 19:37:49 -0800

    update to projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -26,7 +26,7 @@ export default class Projects extends Component {
 					<div>
 						<div className='post'>
 							<a
-								href='hhttps://covid-track-er.web.app/'
+								href='https://covid-track-er.web.app/'
 								target='_blank'
 								rel='noopener noreferrer'>
 								<img
@@ -47,7 +47,7 @@ export default class Projects extends Component {
 								</div>
 								<div className='viewMoreBtn'>
 									<a
-										href='hhttps://covid-track-er.web.app/'
+										href='https://covid-track-er.web.app/'
 										target='_blank'
 										rel='noopener noreferrer'>
 										View More{' '}
